c322242dcaf9fe72df138c9809e771840c2cc566
Davi Santana <santananogueira13@hotmail.com>
2023-05-02 19:42:24 -0300

    router gh

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { UserStorage } from './contexts/userContext';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserStorage>
-      <App />
-    </UserStorage>
+    <Router basename={process.env.PUBLIC_URL}>
+      <UserStorage>
+        <App />
+      </UserStorage>
+    </Router>
   </React.StrictMode>
 );
 
